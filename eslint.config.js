6dd3c16835a8a6ecd8af5a50f5eb98a2b25b1149
Nikita Lobanov <nikitaalobanovv@gmail.com>
2026-01-19 11:29:07 -0800

    chore: migrate from next lint to ESLint CLI
    - Replace deprecated 'next lint' with 'eslint .'
    - Update eslint.config.js with proper ignores for config files
    - Re-add @eslint/eslintrc for FlatCompat

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "next-env.d.ts", "*.config.js", "*.config.ts"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
